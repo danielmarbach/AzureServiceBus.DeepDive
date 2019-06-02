121ea9a807c6692a7de4d95cf78b7a8a95827b46
danielmarbach <daniel.marbach@openplace.net>
2019-06-02 12:22:12 +0200

    Send demo

---
+++ Send/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace Send
+{
+    class Program
+    {
+        static string connectionString = Environment.GetEnvironmentVariable("AzureServiceBus_ConnectionString", EnvironmentVariableTarget.Machine);
+        static string destination = "queue";
+
+        static async Task Main(string[] args)
+        {
+            var client = new QueueClient(connectionString, destination);
+            try
+            {
+                var message = new Message();
+                message.Body = Encoding.UTF8.GetBytes("Deep Dive");
+
+                message.UserProperties.Add("TenantId", "MyTenantId");
+                // explore a few more properties
+
+                await client.SendAsync(message);
+            }
+            finally
+            {
+                await client.CloseAsync();
+            }
+        }
+    }
+}
