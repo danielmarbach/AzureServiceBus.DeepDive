29caf7655e5d8643c4932b9bb5a0b3a40f40d573
danielmarbach <daniel.marbach@openplace.net>
2019-06-02 12:35:15 +0200

    Configure SB Explorer

--- ServiceBusExplorer.ps1
+++ ServiceBusExplorer.ps1
@@ -4,3 +4,12 @@ $outputFile = "$PSScriptRoot\ServiceBusExplorer.zip"
 (New-Object System.Net.WebClient).DownloadFile($url, $outputFile)
 Expand-Archive $outputFile -DestinationPath "$PSScriptRoot\explorer" -Force
 Remove-Item $outputFile -Force
+$xmlPath = "$PSScriptRoot\explorer\ServiceBusExplorer.exe.Config"
+Copy-Item -Path ServiceBusExplorer.exe.Config -Destination $xmlPath -Force
+$xml=New-Object XML
+$xml.Load($xmlPath)
+$nodes = $xml.SelectNodes('/configuration/serviceBusNamespaces/add[@key="ASB_DEEP_DIVE"]');
+foreach($node in $nodes) {
+    $node.SetAttribute("value", $env:AzureServiceBus_ConnectionString);
+}
+$xml.Save($xmlPath)
