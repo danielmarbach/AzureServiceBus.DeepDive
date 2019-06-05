a2149211b4bd5f849b9871d6fad9cfadd8a2f604
danielmarbach <daniel.marbach@openplace.net>
2019-06-05 20:54:39 +0200

    Expiry demo

---
+++ Expiry/Prepare.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Management;
+
+namespace Expiry
+{
+    public static class Prepare
+    {
+        public static async Task Stage(string connectionString, string destination)
+        {
+            var client = new ManagementClient(connectionString);
+            if (await client.QueueExistsAsync(destination))
+            {
+                await client.DeleteQueueAsync(destination);
+            }
+
+            var description = new QueueDescription(destination)
+            {
+                MaxDeliveryCount = int.MaxValue,
+            };
+            await client.CreateQueueAsync(description);
+
+            await client.CloseAsync();
+        }
+
+        public static async Task SimulateActiveReceiver(QueueClient client)
+        {
+            client.RegisterMessageHandler(
+            async (message, token) =>
+            {
+                await client.AbandonAsync(message.SystemProperties.LockToken);
+                await Task.Delay(2000, token);
+            },
+            new MessageHandlerOptions(exception => Task.CompletedTask)
+            {
+                AutoComplete = false,
+            });
+
+            await Task.Delay(TimeSpan.FromSeconds(15));
+        }
+    }
+}
