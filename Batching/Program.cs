6b698ec3f8f1fb135488cced97403fdd424e5023
danielmarbach <daniel.marbach@openplace.net>
2019-06-11 21:11:48 +0200

    QuotaExceeded

--- Batching/Program.cs
+++ Batching/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 using Microsoft.Azure.ServiceBus;
 
 namespace Batching
@@ -16,6 +17,7 @@ namespace Batching
             await Prepare.Stage(connectionString, destination);
 
             var client = new QueueClient(connectionString, destination);
+
             var messages = new List<Message>();
             for (int i = 0; i < 10; i++)
             {
@@ -32,7 +34,38 @@ namespace Batching
                 message.Body = Encoding.UTF8.GetBytes($"Deep Dive{i}");
                 messages.Add(message);
             }
-            await client.SendAsync(messages);
+            try
+            {
+                await client.SendAsync(messages);
+            }
+            catch (MessageSizeExceededException ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+
+            messages.Clear();
+            Console.WriteLine();
+
+            for (int i = 0; i < 101; i++)
+            {
+                var message = new Message();
+                message.Body = Encoding.UTF8.GetBytes($"Deep Dive{i}");
+                messages.Add(message);
+            }
+
+            try
+            {
+                using (var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+                {
+                    await client.SendAsync(messages);
+                    scope.Complete();
+                }
+            }
+            catch (QuotaExceededException ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+
         }
     }
 }
