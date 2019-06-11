7188cc5d57385ee3aecc748c24817ef81a4668dc
danielmarbach <daniel.marbach@openplace.net>
2019-06-11 20:58:00 +0200

    AtomicSend

---
+++ AtomicSend/Prepare.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus.Management;
+
+namespace AtomicSend
+{
+    public static class Prepare
+    {
+        public static async Task Stage(string connectionString, string destination)
+        {
+            var client = new ManagementClient(connectionString);
+            if (await client.QueueExistsAsync(destination))
+            {
+                await client.DeleteQueueAsync(destination);
+            }
+            await client.CreateQueueAsync(destination);
+            await client.CloseAsync();
+        }
+
+        public static async Task ReportNumberOfMessages(string connectionString, string destination)
+        {
+            var client = new ManagementClient(connectionString);
+
+            var info = await client.GetQueueRuntimeInfoAsync(destination);
+
+            await client.CloseAsync();
+
+            Console.WriteLine($"#'{info.MessageCount}' messages in '{destination}'");
+        }
+    }
+}
