e7cb3ff306fde3df63f20ba9c0d463fdee3e1af5
danielmarbach <daniel.marbach@openplace.net>
2019-06-11 22:02:52 +0200

    SendVia

---
+++ SendVia/Prepare.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus.Management;
+
+namespace SendVia
+{
+    public static class Prepare
+    {
+        public static async Task Stage(string connectionString, string inputQueue, string destinationQueue)
+        {
+            var client = new ManagementClient(connectionString);
+            if (await client.QueueExistsAsync(inputQueue))
+            {
+                await client.DeleteQueueAsync(inputQueue);
+            }
+            await client.CreateQueueAsync(new QueueDescription(inputQueue) { MaxDeliveryCount = 2 });
+
+            if (await client.QueueExistsAsync(destinationQueue))
+            {
+                await client.DeleteQueueAsync(destinationQueue);
+            }
+            await client.CreateQueueAsync(destinationQueue);
+
+            await client.CloseAsync();
+        }
+
+        public static async Task ReportNumberOfMessages(string connectionString, string destination)
+        {
+            var client = new ManagementClient(connectionString);
+
+            var info = await client.GetQueueRuntimeInfoAsync(destination);
+
+            await client.CloseAsync();
+
+            Console.WriteLine($"#'{info.MessageCount}' messages in '{destination}'");
+        }
+    }
+}
