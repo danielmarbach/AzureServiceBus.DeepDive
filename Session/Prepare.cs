1f481916b9e9dd7127f412afdac7d912e1785ea6
danielmarbach <daniel.marbach@openplace.net>
2019-06-12 22:32:14 +0200

    Not delete

---
+++ Session/Prepare.cs
@@ -0,0 +1,23 @@
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus.Management;
+
+namespace Session
+{
+    public static class Prepare
+    {
+        public static async Task Stage(string connectionString, string destination)
+        {
+            var client = new ManagementClient(connectionString);
+            if (await client.QueueExistsAsync(destination))
+            {
+                await client.DeleteQueueAsync(destination);
+            }
+            var queueDescription = new QueueDescription(destination)
+                {
+                    RequiresSession = true
+                };
+                await client.CreateQueueAsync(queueDescription);
+            await client.CloseAsync();
+        }
+    }
+}
