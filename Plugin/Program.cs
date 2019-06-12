e50416c0e8e1e64af89c0c43aaa0cc6bc035be6c
Sean Feldman <SeanFeldman@users.noreply.github.com>
2019-06-12 15:07:53 -0600

    Update Program.cs

--- Plugin/Program.cs
+++ Plugin/Program.cs
@@ -20,7 +20,7 @@ namespace Send
             public override Task<Message> BeforeMessageSend(Message message)
             {
                 var currentBody = Encoding.UTF8.GetString(message.Body);
-                message.Body = Encoding.UTF8.GetBytes($"Yes I can before send{Environment.NewLine}{currentBody}");
+                message.Body = Encoding.UTF8.GetBytes($"Yes, I can run before send{Environment.NewLine}{currentBody}");
                 return Task.FromResult(message);
             }
 
