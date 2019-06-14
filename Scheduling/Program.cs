4fe0611007869f86f10584cfb3c4732e7bad7c4f
danielmarbach <daniel.marbach@openplace.net>
2019-06-14 22:05:57 +0200

    DateTime

--- Scheduling/Program.cs
+++ Scheduling/Program.cs
@@ -20,20 +20,20 @@ namespace Scheduling
             var sender = new MessageSender(connectionString, destination);
             var due = DateTimeOffset.UtcNow.AddSeconds(10);
             await sender.ScheduleMessageAsync(new Message(Encoding.UTF8.GetBytes($"Deep Dive + {due}")), due);
-            Console.WriteLine("Message scheduled first");
+            Console.WriteLine($"{DateTimeOffset.UtcNow}: Message scheduled first");
 
             var sequenceId =
                 await sender.ScheduleMessageAsync(new Message(Encoding.UTF8.GetBytes($"Deep Dive + {due}")), due);
-            Console.WriteLine("Message scheduled second");
+            Console.WriteLine($"{DateTimeOffset.UtcNow}: Message scheduled second");
 
             await sender.CancelScheduledMessageAsync(sequenceId);
-            Console.WriteLine("Canceled second");
+            Console.WriteLine($"{DateTimeOffset.UtcNow}: Canceled second");
 
             var receiver = new MessageReceiver(connectionString, destination);
             receiver.RegisterMessageHandler((message, token) =>
             {
                 Console.WriteLine(
-                    $"Received message with '{message.MessageId}' and content '{Encoding.UTF8.GetString(message.Body)}' at '{DateTimeOffset.UtcNow}'");
+                    $"{DateTimeOffset.UtcNow}: Received message with '{message.MessageId}' and content '{Encoding.UTF8.GetString(message.Body)}'");
                 return Task.CompletedTask;
             }, ex => Task.CompletedTask);
 
