0d0048bfd9a56ecbfe0ede2fcc8bed2ce3f2f411
danielmarbach <daniel.marbach@openplace.net>
2021-02-26 11:47:09 +0100

    Receive sample

--- Receive/Prepare.cs
+++ Receive/Prepare.cs
@@ -3,20 +3,40 @@ using Microsoft.Azure.ServiceBus.Management;
 
 namespace Receive
 {
+    using System;
+
     public static class Prepare
     {
-        public static async Task Stage(string connectionString, string destination)
+        public static async Task<IAsyncDisposable> Stage(string connectionString, string destination)
         {
             var client = new ManagementClient(connectionString);
             if (!await client.QueueExistsAsync(destination)) await client.CreateQueueAsync(destination);
             await client.CloseAsync();
+            return new Leave(connectionString, destination);
         }
 
-        public static async Task LeaveStage(string connectionString, string destination)
+        static async Task LeaveStage(string connectionString, string destination)
         {
             var client = new ManagementClient(connectionString);
             await client.DeleteQueueAsync(destination);
             await client.CloseAsync();
+        }
+
+        sealed class Leave : IAsyncDisposable
+        {
+            readonly string connectionString;
+            readonly string destination;
+
+            public Leave(string connectionString, string destination)
+            {
+                this.connectionString = connectionString;
+                this.destination = destination;
+            }
+
+            public async ValueTask DisposeAsync()
+            {
+                await LeaveStage(connectionString, destination);
+            }
         }
     }
 }
