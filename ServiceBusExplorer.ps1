2589d0475c3d8ce73f3950c1eb1ec2d39e69dac6
danielmarbach <daniel.marbach@openplace.net>
2021-03-01 14:35:08 +0100

    Revert "New explorer"
    This reverts commit 357e9a99f3169263b688168567cd48f38fe1fd82.

--- ServiceBusExplorer.ps1
+++ ServiceBusExplorer.ps1
@@ -1,4 +1,4 @@
-$url = "https://github.com/paolosalvatori/ServiceBusExplorer/releases/download/5.0.2/ServiceBusExplorer-5.0.2.zip"
+$url = "https://github.com/paolosalvatori/ServiceBusExplorer/releases/download/4.1.112/ServiceBusExplorer-4.1.112.zip"
 $outputFile = "$PSScriptRoot\ServiceBusExplorer.zip"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 (New-Object System.Net.WebClient).DownloadFile($url, $outputFile)
