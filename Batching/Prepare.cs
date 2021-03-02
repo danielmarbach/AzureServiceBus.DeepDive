124f5a15654cdc7d475e630fdd7736fc8d26f8ea
danielmarbach <daniel.marbach@openplace.net>
2021-03-02 13:45:37 +0100

    Update batching sample

--- Batching/Prepare.cs
+++ Batching/Prepare.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.Azure.ServiceBus.Management;
 
@@ -5,12 +6,36 @@ namespace Batching
 {
     public static class Prepare
     {
-        public static async Task Stage(string connectionString, string destination)
+        public static async Task<IAsyncDisposable> Stage(string connectionString, string destination)
         {
             var client = new ManagementClient(connectionString);
-            if (await client.QueueExistsAsync(destination)) await client.DeleteQueueAsync(destination);
-            await client.CreateQueueAsync(destination);
+            if (!await client.QueueExistsAsync(destination)) await client.CreateQueueAsync(destination);
             await client.CloseAsync();
+            return new Leave(connectionString, destination);
+        }
+
+        static async Task LeaveStage(string connectionString, string destination)
+        {
+            var client = new ManagementClient(connectionString);
+            await client.DeleteQueueAsync(destination);
+            await client.CloseAsync();
+        }
+
+        sealed class Leave : IAsyncDisposable
+        {
+            readonly string connectionString;
+            readonly string destination;
+
+            public Leave(string connectionString, string destination)
+            {
+                this.connectionString = connectionString;
+                this.destination = destination;
+            }
+
+            public async ValueTask DisposeAsync()
+            {
+                await LeaveStage(connectionString, destination);
+            }
         }
     }
 }
