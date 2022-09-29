e306373db1bf0e53c5022d82431a19a2b717d124
danielmarbach <daniel.marbach@openplace.net>
2022-09-29 17:34:11 +0200

    Fiddling

--- SendVia/Prepare.cs
+++ SendVia/Prepare.cs
@@ -6,23 +6,27 @@ namespace SendVia
 {
     public static class Prepare
     {
-        public static async Task Stage(string connectionString, string inputQueue, string destinationQueue)
+        public static async Task Stage(string connectionString, string inputQueue, string destinationQueue, string anotherDestinationQueue)
         {
             var client = new ServiceBusAdministrationClient(connectionString);
             if (await client.QueueExistsAsync(inputQueue)) await client.DeleteQueueAsync(inputQueue);
             await client.CreateQueueAsync(new CreateQueueOptions(inputQueue) { MaxDeliveryCount = 2 });
 
             if (await client.QueueExistsAsync(destinationQueue)) await client.DeleteQueueAsync(destinationQueue);
+            if (await client.QueueExistsAsync(anotherDestinationQueue)) await client.DeleteQueueAsync(anotherDestinationQueue);
             await client.CreateQueueAsync(destinationQueue);
+            await client.CreateQueueAsync(anotherDestinationQueue);
         }
 
-        public static async Task ReportNumberOfMessages(string connectionString, string destination)
+        public static async Task ReportNumberOfMessages(string connectionString, string destination, string anotherDestination)
         {
             var client = new ServiceBusAdministrationClient(connectionString);
 
             QueueRuntimeProperties info = await client.GetQueueRuntimePropertiesAsync(destination);
+            QueueRuntimeProperties anotherInfo = await client.GetQueueRuntimePropertiesAsync(anotherDestination);
 
             Console.WriteLine($"#'{info.ActiveMessageCount}' messages in '{destination}'");
+            Console.WriteLine($"#'{anotherInfo.ActiveMessageCount}' messages in '{anotherDestination}'");
         }
     }
 }
--- SendVia/Program.cs
+++ SendVia/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Transactions;
 using static System.Console;
@@ -15,56 +16,26 @@ namespace SendVia
 
         private static readonly string inputQueue = "queue";
         private static readonly string destinationQueue = "destination";
+        private static readonly string anotherDestinationQueue = "anotherdestination";
 
         private static TaskCompletionSource<bool> syncEvent =
+            new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
+        
+        private static TaskCompletionSource<bool> syncEvent2 =
             new TaskCompletionSource<bool>(TaskCreationOptions.RunContinuationsAsynchronously);
 
         private static async Task Main(string[] args)
         {
-            await Prepare.Stage(connectionString, inputQueue, destinationQueue);
+            await Prepare.Stage(connectionString, inputQueue, destinationQueue, anotherDestinationQueue);
 
-            await using var serviceBusClient = new ServiceBusClient(connectionString, new ServiceBusClientOptions
+            await using var nonTransactionalClient = new ServiceBusClient(connectionString, new ServiceBusClientOptions
             {
                 RetryOptions = new ServiceBusRetryOptions
                 {
                     TryTimeout = TimeSpan.FromSeconds(2)
                 }
             });
-            await using var kickOffSender = serviceBusClient.CreateSender(inputQueue);
-
-            await kickOffSender.SendMessageAsync(new ServiceBusMessage("Kick off"));
-
-            var receiver = serviceBusClient.CreateProcessor(inputQueue);
-            var sender = serviceBusClient.CreateSender(destinationQueue);
-
-            await Prepare.ReportNumberOfMessages(connectionString, destinationQueue);
-
-            receiver.ProcessMessageAsync += async processMessageEventArgs =>
-            {
-                var message = processMessageEventArgs.Message;
-
-                await Error.WriteLineAsync(
-                    $"Received message with '{message.MessageId}' and content '{UTF8.GetString(message.Body)}'");
-                await sender.SendMessageAsync(new ServiceBusMessage("Will leak"));
-                throw new InvalidOperationException();
-            };
-            receiver.ProcessErrorAsync += _ => Prepare.ReportNumberOfMessages(connectionString, destinationQueue);
-
-            await receiver.StartProcessingAsync();
-
-            ReadLine();
-            await receiver.StopProcessingAsync();
-            await receiver.CloseAsync();
-            await sender.CloseAsync();
-
-            await Prepare.Stage(connectionString, inputQueue, destinationQueue);
-
-            await kickOffSender.SendMessageAsync(new ServiceBusMessage("Fail"));
-            var winMessage = new ServiceBusMessage("Win");
-            winMessage.ApplicationProperties.Add("Win", true);
-            await kickOffSender.SendMessageAsync(winMessage);
-            await kickOffSender.CloseAsync();
-
+            
             await using var transactionalClient = new ServiceBusClient(connectionString, new ServiceBusClientOptions
             {
                 EnableCrossEntityTransactions = true,
@@ -73,8 +44,19 @@ namespace SendVia
                     TryTimeout = TimeSpan.FromSeconds(2)
                 }
             });
-            receiver = transactionalClient.CreateProcessor(inputQueue);
-            sender = transactionalClient.CreateSender(destinationQueue);
+            
+            await using var kickOffSender = nonTransactionalClient.CreateSender(inputQueue);
+            await kickOffSender.SendMessageAsync(new ServiceBusMessage("Fail"));
+            var winMessage = new ServiceBusMessage("Win");
+            winMessage.ApplicationProperties.Add("Win", true);
+            await kickOffSender.SendMessageAsync(winMessage);
+            await kickOffSender.CloseAsync();
+            
+            var receiver = transactionalClient.CreateProcessor(inputQueue);
+            var sender = transactionalClient.CreateSender(destinationQueue);
+            var anotherSender = transactionalClient.CreateSender(anotherDestinationQueue);
+
+            await anotherSender.SendMessagesAsync(Enumerable.Range(0, 10).Select(i => new ServiceBusMessage(i.ToString())));
 
             receiver.ProcessMessageAsync += async processMessageEventArgs =>
             {
@@ -89,14 +71,31 @@ namespace SendVia
 
                     await sender.SendMessageAsync(new ServiceBusMessage("Will not leak"));
 
+                    await Prepare.ReportNumberOfMessages(connectionString, destinationQueue, anotherDestinationQueue);
+                    
+                    syncEvent.SetResult(true);
+                    
+                    await syncEvent2.Task;
+
                     scope.Complete();
                 }
 
-                await Prepare.ReportNumberOfMessages(connectionString, destinationQueue);
+                await Prepare.ReportNumberOfMessages(connectionString, destinationQueue, anotherDestinationQueue);
             };
-            receiver.ProcessErrorAsync += _ => Prepare.ReportNumberOfMessages(connectionString, destinationQueue);
-
+            receiver.ProcessErrorAsync += _ => Prepare.ReportNumberOfMessages(connectionString, destinationQueue, anotherDestinationQueue);
+            
             await receiver.StartProcessingAsync();
+
+            await syncEvent.Task;
+            
+            using (var scope = new TransactionScope(TransactionScopeOption.Suppress, TransactionScopeAsyncFlowOption.Enabled))
+            {
+                await anotherSender.SendMessagesAsync(Enumerable.Range(11, 10).Select(i => new ServiceBusMessage(i.ToString())));
+                
+                scope.Complete();
+            }
+            
+            syncEvent2.SetResult(true);
 
             ReadLine();
 
